43c24a5755decf6ed4650826f954501aa93c8636
UnShame <mrunshame@gmail.com>
2018-03-31 02:49:51 +0300

    Minor refactoring

--- Assets/Scripts/BuildStation.cs
+++ Assets/Scripts/BuildStation.cs
@@ -36,9 +36,11 @@ public class Block {
     }
 
     public void fill(GameObject block, bool collide, Vector3 offset, List<Block> affectedBlocks = null) {
+
         if (this.block) {
             hide();
         }
+
         this.block = block;
 
         if(affectedBlocks == null) {
@@ -52,6 +54,7 @@ public class Block {
         isFilled = true;
 
         this.affectedBlocks = affectedBlocks;
+        
         foreach (Block affectedBlock in affectedBlocks) {
             affectedBlock.fill();
         }
@@ -64,6 +67,7 @@ public class Block {
     }
 
     public void empty(GameObject block) {
+
         if (this.block == block) {
             this.block = null;
             isFilled = false;
@@ -73,14 +77,17 @@ public class Block {
 
     private void emptyAffected() {
         if (affectedBlocks == null) return;
+
         foreach (Block affectedBlock in affectedBlocks) {
             affectedBlock.empty();
         }
+
         affectedBlocks = null;
     }
 
     public void hide() {
         if (!block) return;
+
         block.GetComponent<Rigidbody>().isKinematic = true;
         block.GetComponent<Renderer>().enabled = false;
         block.GetComponent<Collider>().enabled = false;
@@ -88,6 +95,7 @@ public class Block {
 
     public void show(bool collide = false) {
         if (!block) return;
+
         block.GetComponent<Rigidbody>().isKinematic = true;
         block.GetComponent<Renderer>().enabled = true;
         block.GetComponent<Collider>().enabled = collide;
@@ -107,9 +115,10 @@ public class BuildStation : MonoBehaviour {
 
     public int size = 10;
     public float blockSize = 1;
+
     public GameObject brush;
-    private Block brushBlock = null;
     public Material brushMaterial;
+    private Block brushBlock = null;
 
     private List<List<List<Block>>> blocks = new List<List<List<Block>>>();
     private List<GameObject> blocksList = new List<GameObject>();
@@ -121,24 +130,39 @@ public class BuildStation : MonoBehaviour {
         brush.GetComponent<Rigidbody>().isKinematic = true;
         brush.GetComponent<Renderer>().enabled = false;
         brush.GetComponent<Collider>().enabled = false;
-        //blockSize = baseBlock.GetComponent<Renderer>().bounds.size.x;
+
         var offset = size * blockSize / 2;
         var position = transform.position - new Vector3(offset, offset, offset);
+
         for (int x = 0; x < size; x++) {
             var dimension = new List<List<Block>>();
+
             for (int y = 0; y < size; y++) {
                 var axis = new List<Block>();
+
                 for (int z = 0; z < size; z++) {
-                    var block = new Block(new Vector3(position.x + x * blockSize, position.y + y * blockSize, position.z + z * blockSize), Quaternion.identity, transform.parent);
+
+                    var block = new Block(
+                        new Vector3(
+                            position.x + x * blockSize, 
+                            position.y + y * blockSize, 
+                            position.z + z * blockSize), 
+                        Quaternion.identity, 
+                        transform.parent
+                    );
+
                     axis.Add(block);
                 }
+
                 dimension.Add(axis);
             }
+
             blocks.Add(dimension);
         }
     }
 
     void HideBrush() {
+
         if (brushBlock != null) {
             brushBlock.hide();
             brushBlock.empty();
@@ -157,13 +181,16 @@ public class BuildStation : MonoBehaviour {
     }
 
     Block GetBlock(Coord coord) {
+
         if(coord.x >= size || coord.y >= size || coord.z >= size) {
             return null;
         }
+
         return blocks[coord.x][coord.y][coord.z];
     }
 
     void RemoveBlock(GameObject otherBlock) {
+
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
                 for (int z = 0; z < size; z++) {
@@ -172,14 +199,16 @@ public class BuildStation : MonoBehaviour {
                 }
             }
         }
+
         blocksList.Remove(otherBlock);
+
         if (otherBlock.transform.parent == transform.parent) {
             otherBlock.transform.parent = null;
         }
     }
 
     void AddBlock(Coord blockCoord, GameObject otherBlock, Vector3 offset, List<Block> affectedBlocks) {
-        var block = blocks[blockCoord.x][blockCoord.y][blockCoord.z];
+        var block = GetBlock(blockCoord);
         otherBlock.transform.parent = transform.parent;
         blocksList.Add(otherBlock);
         block.fill(otherBlock, true, offset, affectedBlocks);
@@ -193,6 +222,7 @@ public class BuildStation : MonoBehaviour {
         if (!interactible) {
             return;
         }
+
         if (blocksList.Contains(otherBlock)) {
             if (interactible.isActive) {
                 RemoveBlock(otherBlock);
@@ -203,22 +233,27 @@ public class BuildStation : MonoBehaviour {
         }
 
         HideBrush();
+
         var minDist = Mathf.Infinity;
         Coord minBlockCoord = new Coord();
         List<Block> minAffectedBlocks = null;
+        Block closestBlock = null;
+
         var otherCollider = other.GetComponent<BoxCollider>();
         var offset = transform.rotation * (new Vector3(otherCollider.size.x, otherCollider.size.y, otherCollider.size.z) * blockSize / 2);
         var otherPosition = other.transform.position - offset;
-        Block closestBlock = null;
+
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
                 for (int z = 0; z < size; z++) {
                     var blockCoord = new Coord(x, y, z);
                     List<Block> affectedBlocks;
+
                     var block = GetValidBlock(blockCoord, otherBlock.GetComponent<BoxCollider>().size, out affectedBlocks);
                     if (block == null) {
                         continue;
                     }
+
                     var dist = Vector3.Distance(otherPosition, block.position);
                     if (dist < blockSize*4 && dist < minDist) {
                         closestBlock = block;
@@ -229,6 +264,7 @@ public class BuildStation : MonoBehaviour {
                 }
             }
         }
+
         if (closestBlock != null) {
             if (interactible.isActive) {
                 ShowBrush(closestBlock, otherBlock.GetComponent<MeshFilter>().mesh, offset);
@@ -239,9 +275,14 @@ public class BuildStation : MonoBehaviour {
         }
     }
 
+    void OnTriggerExit(Collider other) {
+        HideBrush();
+    }
+
     Block GetValidBlock(Coord blockCoord, Vector3 size, out List<Block> affectedBlocks) {
         var block = GetBlock(blockCoord);
         var isValid = false;
+
         if (blockCoord.y == 0) {
             isValid = true;
         }
@@ -253,11 +294,13 @@ public class BuildStation : MonoBehaviour {
                 }
             }
         }
+
         var sizeCoord = new Coord(
             blockCoord.x + Mathf.CeilToInt(size.x) - 1, 
             blockCoord.y + Mathf.CeilToInt(size.y) - 1, 
             blockCoord.z + Mathf.CeilToInt(size.z) - 1
         );
+
         affectedBlocks = new List<Block>();
 
         if (isValid) {
@@ -268,6 +311,7 @@ public class BuildStation : MonoBehaviour {
         }
 
         isValid = isValid && (block.isEmpty || block.sameAs(brush));
+        
         return isValid ? block : null;
     }
 
@@ -275,7 +319,9 @@ public class BuildStation : MonoBehaviour {
         Block affectedBlock = null;
         var i = dimension == 1 ? sizeCoord.x : (dimension == 2 ? sizeCoord.y : sizeCoord.z);
         var cmpi = dimension == 1 ? blockCoord.x : (dimension == 2 ? blockCoord.y : blockCoord.z);
+
         while (i > cmpi) {
+
             if(dimension == 1) {
                 affectedBlock = GetBlock(i, sizeCoord.y, sizeCoord.z);
             }
@@ -294,6 +340,7 @@ public class BuildStation : MonoBehaviour {
                     return false;
                 }
             }
+
             i--;
         }
         return true;
@@ -301,28 +348,31 @@ public class BuildStation : MonoBehaviour {
 
     List<Block> GetConnectedBlocks(int x, int y, int z) {
         List<Block> connectedBlocks = new List<Block>();
+
         if (x > 0) {
             connectedBlocks.Add(blocks[x - 1][y][z]);
         }
+
         if (x < size - 1) {
             connectedBlocks.Add(blocks[x + 1][y][z]);
         }
+
         if (y > 0) {
             connectedBlocks.Add(blocks[x][y - 1][z]);
         }
+
         if (y < size - 1) {
             connectedBlocks.Add(blocks[x][y + 1][z]);
         }
+
         if (z > 0) {
             connectedBlocks.Add(blocks[x][y][z - 1]);
         }
+
         if (z < size - 1) {
             connectedBlocks.Add(blocks[x][y][z + 1]);
         }
-        return connectedBlocks;
-    }
 
-    private void OnTriggerExit(Collider other) {
-        HideBrush();
+        return connectedBlocks;
     }
 }
